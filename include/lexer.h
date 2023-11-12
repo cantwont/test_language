110eac98ce0b09f9d278416a7bca595f91bcba33
cantwont <ariamath@tutanota.com>
2023-11-11 19:15:11 -0800

    test Some Jawn

--- include/lexer.h
+++ include/lexer.h
@@ -4,5 +4,26 @@
 
 #ifndef LEXER_H
 #define LEXER_H
+#include <stddef.h>
+
+typedef enum {
+    TOKEN_IDENTIFIER,
+    TOKEN_NUMBER,
+    TOKEN_EOF,
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char* lexeme;
+} Token;
+
+typedef struct {
+    const char* source_code;
+    size_t current_position;
+} Lexer;
+
+Lexer* init_lexer(const char* source_code);
+Token get_next_token(Lexer* lexer);
+void destroy_lexer(Lexer* lexer);
 
 #endif //LEXER_H
--- src/lexer.c
+++ src/lexer.c
@@ -1,3 +1,38 @@
 //
 // Created by gabriel on 11/11/2023.
 //
+
+#include "../include/lexer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+typedef enum {
+    TOKEN_IDENTIFIER,
+    TOKEN_NUMBER,
+    TOKEN_EOF,
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char* lexeme;
+} Token;
+
+Lexer* init_lexer(const char* source_code) {
+    Lexer* lexer = (Lexer*)malloc(sizeof(Lexer));
+    lexer->source_code = source_code;
+    lexer->current_position = 0;
+    return lexer;
+}
+
+Token get_next_token(Lexer* lexer) {
+    Token token;
+    token.type = TOKEN_EOF;
+    token.lexeme = NULL;
+
+    return token;
+}
+
+void destroy_lexer(Lexer* lexer) {
+    free(lexer);
+}
