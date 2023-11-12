772754f3bdd9f65eab34691cd5b4015a49ae213c
cantwont <ariamath@tutanota.com>
2023-11-11 19:18:40 -0800

    fix somethingh

--- src/lexer.c
+++ src/lexer.c
@@ -3,20 +3,7 @@
 //
 
 #include "../include/lexer.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
-typedef enum {
-    TOKEN_IDENTIFIER,
-    TOKEN_NUMBER,
-    TOKEN_EOF,
-} TokenType;
-
-typedef struct {
-    TokenType type;
-    char* lexeme;
-} Token;
 
 Lexer* init_lexer(const char* source_code) {
     Lexer* lexer = (Lexer*)malloc(sizeof(Lexer));
